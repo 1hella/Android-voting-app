fe1217cd14d838bb91c26008aa60d0adbe6ec022
Junho Kim <jnhkm311@gmail.com>
2018-04-22 20:01:27 -0700

    Display pie charts in a scroll/linear view
    - Implemented MpPieCharts with Scrollable/LinearLayout

--- app/src/main/java/com/votingapp/votingapp/fragment/PollFragment.java
+++ app/src/main/java/com/votingapp/votingapp/fragment/PollFragment.java
@@ -1,5 +1,7 @@
 package com.votingapp.votingapp.fragment;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -8,19 +10,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.votingapp.votingapp.R;
 import com.votingapp.votingapp.manager.ServerManager;
 import com.votingapp.votingapp.model.Poll;
 import com.votingapp.votingapp.model.PollCollection;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PollFragment extends Fragment {
     private final String TAG = "PollFragment";
 
-    private ListView listView;
+    private LinearLayout pollContainer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,7 +44,7 @@ public class PollFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.poll_fragment_view, container, false);
 
-        setupListView(view);
+        setupPollContainer(view);
 
         return view;
     }
@@ -45,45 +57,105 @@ public class PollFragment extends Fragment {
     }
 
     private void setup() {
-        ServerManager.getAllPolls(
-                getActivity(),
-                this::responseSetup
-        );
+        if (PollCollection.getInstance().getPolls() != null) {
+            populatePollContainer();
+        } else {
+            ServerManager.getAllPolls(
+                    getActivity(),
+                    this::responseSetup
+            );
+        }
     }
 
     private void responseSetup(List<Poll> polls) {
         PollCollection.getInstance().setPolls(polls);
-        populateListView();
+        populatePollContainer();
     }
 
-    private void setupListView(View view) {
-        listView = view.findViewById(R.id.listView);
+    private void setupPollContainer(View view) {
+       pollContainer = view.findViewById(R.id.list_of_polls_container);
     }
 
-    private void populateListView(){
-        ArrayAdapter adapter = new ArrayAdapter<String>(
-                getActivity(),
-                R.layout.listview_text,
-                getMessageDescriptions(PollCollection.getInstance().getPolls()));
+    private void populatePollContainer(){
+        List<Poll> polls = PollCollection.getInstance().getPolls();
 
-        listView.setAdapter(adapter);
+        for (Poll poll : polls) {
+            PieChart chart = buildPieChart(poll);
+
+            PieDataSet dataSet = buildPieDataSet(poll, "");
+
+            PieData pieData = buildPieData(dataSet);
+
+            editPieLegend(chart);
+
+            chart.setData(pieData);
+
+            displayPieChart(chart);
+        }
     }
 
-    public String[] getMessageDescriptions(List<Poll> polls) {
-        String[] descriptions = new String[polls.size()];
-        int i = 0;
+    private void displayPieChart(PieChart chart) {
+        chart.invalidate();
 
-        for (Poll poll: polls) {
-            if (poll.getAuthor() == null) {
-                descriptions[i] = getString(R.string.error_msg);
-            }
-            else {
-                descriptions[i] = poll.getAuthor();
-            }
+        pollContainer.addView(
+                chart,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                1050);
+    }
 
-            i++;
+    private void editPieLegend(PieChart chart) {
+        Legend legend = chart.getLegend();
+
+        legend.setFormSize(10f);
+        legend.setTextSize(12f);
+        legend.setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setWordWrapEnabled(true);
+        legend.setYEntrySpace(5f);
+        legend.setXEntrySpace(5f);
+    }
+
+    private PieData buildPieData(PieDataSet dataSet) {
+        PieData pieData = new PieData(dataSet);
+
+        pieData.setValueTextSize(10f);
+        pieData.setValueTextColor(Color.BLACK);
+
+        return pieData;
+    }
+
+    private PieDataSet buildPieDataSet(Poll poll, String label) {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+
+        for (int i = 0; i < poll.getOptions().size() ; i++) {
+            entries.add(new PieEntry(
+                    poll.getVotes().get(i),
+                    poll.getOptions().get(i)));
         }
 
-        return descriptions;
+        PieDataSet dataSet = new PieDataSet(entries, label);
+
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        return dataSet;
+    }
+
+    private PieChart buildPieChart(Poll poll) {
+        PieChart chart = new PieChart(getActivity());
+
+        chart.getDescription().setEnabled(false);
+        chart.setUsePercentValues(true);
+        chart.setRotationEnabled(true);
+        chart.setDrawHoleEnabled(true);
+        chart.setDrawSliceText(false);
+        chart.setCenterText(poll.getName());
+        chart.setHoleColor(Color.WHITE);
+        chart.setHoleRadius(50f);
+        chart.setTransparentCircleRadius(30f);
+        chart.setDragDecelerationFrictionCoef(0.99f);
+
+        return chart;
     }
 }
